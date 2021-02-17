05793131c4377e6123966932a7ea33632b2e27cd
singlelogic <singlelogic@gmail.com>
2021-02-17 13:11:20 +0300

    Connection of the project to the database PostgreSQL

--- inportal/settings.py
+++ inportal/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'inportal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'inportal',
+        'USER': 'djangouser',
+        'PASSWORD': os.getenv('SECRET_KEY_DB'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
