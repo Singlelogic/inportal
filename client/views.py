bb2076560c2d473cc0c478b3919bf8bad1d98430
singlelogic <singlelogic@gmail.com>
2021-02-18 16:57:26 +0300

    Added 'LoginRequiredMixin' for client app

--- client/views.py
+++ client/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -5,7 +6,7 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Client
 
 
-class ClientCreateView(CreateView):
+class ClientCreateView(LoginRequiredMixin, CreateView):
     """Create client."""
     model = Client
     template_name = 'client/client_create.html'
@@ -13,12 +14,12 @@ class ClientCreateView(CreateView):
     success_url = reverse_lazy('list_client_url')
 
 
-class ClientListView(ListView):
+class ClientListView(LoginRequiredMixin, ListView):
     """List of clients."""
     model = Client
 
 
-class ClientUpdateView(UpdateView):
+class ClientUpdateView(LoginRequiredMixin, UpdateView):
     """Update the selected client."""
     model = Client
     template_name = 'client/client_update.html'
@@ -26,7 +27,7 @@ class ClientUpdateView(UpdateView):
     success_url = reverse_lazy('list_client_url')
 
 
-class ClientDeleteView(DeleteView):
+class ClientDeleteView(LoginRequiredMixin, DeleteView):
     """Delete the selected client."""
     model = Client
     template_name = 'client/client_delete.html'
