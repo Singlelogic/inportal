6296f3c335fa820ab497cebc9c2598b685db4b49
singlelogic <singlelogic@gmail.com>
2021-04-12 16:28:23 +0300

    Added help_text for DataCollectTerminal forms

--- dct/forms.py
+++ dct/forms.py
@@ -24,3 +24,7 @@ class DataCollectTerminalUpdateForm(forms.ModelForm):
         widgets = {
             'name': forms.TextInput(attrs={'placeholder': 'ТСД-1'}),
         }
+        help_texts = {
+            'accumulator': 'Исключены списанные и привязанные к другим ТСД аккумуляторы',
+            'user': 'Исключены пользователи, к которым привязаны другие ТСД'
+        }
